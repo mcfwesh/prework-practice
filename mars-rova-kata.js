09339ae6e36823141aef6d66d5d5f56140a414f0
Nate <mcfwesh@gmail.com>
2020-02-14 22:58:38 +0100

    correction on coordinates

--- mars-rova-kata.js
+++ mars-rova-kata.js
@@ -1,7 +1,7 @@
 let marsRover = {
-  direction: "W",
+  direction: "",
   x: 1,
-  y: 10,
+  y: 1,
   travelLog: [{x: 0, y: 0}]
 }
 
@@ -10,20 +10,22 @@ if (command === "l") {
   switch (rover.direction){
   case "N":
       rover.direction = 'W';
-      console.log(`Rover moves to ${rover.direction}`);
+      console.log(`Rover turns to ${rover.direction}`);
   break;
   case "W":
       rover.direction = 'S';
-      console.log(`Rover moves to ${rover.direction}`);
+      console.log(`Rover turns to ${rover.direction}`);
     break;
     case "S":
         rover.direction = 'E';
-        console.log(`Rover moves to ${rover.direction}`);
+        console.log(`Rover turns to ${rover.direction}`);
         break;
       case "E":
           rover.direction = 'N';
-          console.log(`Rover moves to ${rover.direction}`);
+          console.log(`Rover turns to ${rover.direction}`);
           break;
+          default:
+            console.log('Input the right direction');
 }
 }
 return rover.direction;
@@ -35,20 +37,22 @@ if (command === "r") {
     switch (rover.direction){
     case "N":
       rover.direction = 'E';
-    console.log(`Rover moves to ${rover.direction}`);
+    console.log(`Rover turns to ${rover.direction}`);
     break;
     case "E":
         rover.direction = 'S';
-        console.log(`Rover moves to ${rover.direction}`);
+        console.log(`Rover turns to ${rover.direction}`);
       break;
       case "S":
           rover.direction = 'W';
-          console.log(`Rover moves to ${rover.direction}`);
+          console.log(`Rover turns to ${rover.direction}`);
           break;
         case "W":
             rover.direction = 'N';
-            console.log(`Rover moves to ${rover.direction}`);
+            console.log(`Rover turns to ${rover.direction}`);
             break;
+            default:
+            console.log('Input the right direction');
   }
 }
 return rover.direction;
@@ -72,43 +76,42 @@ if ((rover.x >= 0 && rover.x <= 9) && (rover.y >= 0 && rover.y <= 9)){
 switch (rover.direction){
   case "N":
     if (rover.x <= 9 && rover.y === 0){
-      console.log("out!")
+      rover.y;
     } else {
       rover.y--;
     }
     break;
     case "S":
         if (rover.x <= 9 && rover.y === 9){
-          console.log("out!")
+          rover.y;
         } else {
           rover.y++;
         }
         break;
         case "E":
             if (rover.x === 9 && rover.y <= 9){
-              console.log("out!")
+              rover.x;
             } else {
               rover.x++;
             }
             break;
             case "W":
                 if (rover.x === 0 && rover.y <= 9){
-                  console.log("out!")
-                } else {
+                  rover.x;
+                }else{
                   rover.x--;
                 }
                 break;
 }
-console.log(`The current position is x:${rover.x} and y: ${rover.y}`);
-} else{
-  console.log("Enter a valid coordinate!");
-}
-}
+console.log(`Turns ${rover.direction}. The new position is x:${rover.x} and y: ${rover.y}`);
 let newPosition = {x: rover.x, y: rover.y};
-rover.travelLog.push(newPosition);  
+rover.travelLog.push(newPosition);
+} else{
+  console.log('Invalid coordinates')
+}
+}
 
 }
-moveForward(marsRover, 'f')
 
 function moveBackward(rover, command){
   if (command === "b"){
@@ -116,41 +119,42 @@ function moveBackward(rover, command){
     switch (rover.direction){
       case "N":
         if (rover.x <= 9 && rover.y === 0){
-          console.log("out!")
+          rover.y;
         } else {
           rover.y++;
         }
         break;
         case "S":
             if (rover.x <= 9 && rover.y === 9){
-              console.log("out!")
+              rover.y;
             } else {
               rover.y--;
             }
             break;
             case "E":
                 if (rover.x === 9 && rover.y <= 9){
-                  console.log("out!")
+                  rover.x;
                 } else {
                   rover.x--;
                 }
                 break;
                 case "W":
                     if (rover.x === 0 && rover.y <= 9){
-                      console.log("out!")
-                    } else {
+                      rover.x;
+                    }else{
                       rover.x++;
                     }
                     break;
     }
-    console.log(`The current position is x:${rover.x} and y: ${rover.y}`);
+    console.log(`Turns ${rover.direction}. The current position is x:${rover.x} and y: ${rover.y}`);
+    let newPosition = {x: rover.x, y: rover.y};
+    rover.travelLog.push(newPosition);
     } else{
-      console.log("Enter a valid coordinate!");
-    } 
+      console.log('Invalid coordinates')
     }
-let newPosition = {x: rover.x, y: rover.y};
-rover.travelLog.push(newPosition);
-}
+    }
+    
+    }
 function allDirections (rover, commandSet){
 for (let i = 0; i < commandSet.length; i++){
   let eachCommand = commandSet[i];
@@ -168,9 +172,14 @@ for (let i = 0; i < commandSet.length; i++){
             turnLeft(rover, eachCommand);
             break;
   }
+
 }
+
+  }
+
+allDirections(marsRover, "f");
+for (let i = 0; i < marsRover.travelLog.length; i++){
+  console.log(`Path ${i}: x= ${marsRover.travelLog[i].x}, y= ${marsRover.travelLog[i].y}`)
 }
-// allDirections(marsRover, "f");
-// for (let i = 0; i < marsRover.travelLog.length; i++){
-//   console.log(`Path ${i}: x= ${marsRover.travelLog[i].x}, y= ${marsRover.travelLog[i].y}`)
-// }
+
+//obstacles
